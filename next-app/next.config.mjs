8db1928f20f99f18dfadd9e7350e41f5cd3e162c
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-03-03 17:44:22 +0100

    Fix build error

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  //vercel does not have access to my server directory, therefore it does not have the types from the backend bruh
-  ignoreBuildErrors: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
