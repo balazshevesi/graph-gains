6a222bd786746ade8de8d6641df5a915b5ffbfc2
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-04-13 14:38:05 +0200

    Edit next config

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  webpack: (config, { defaultLoaders }) => {
+    // Push a new rule for TypeScript files
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true, // Skip type checking
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
